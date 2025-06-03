73c93815f4cc12b07ec4f2d4e4f6ddefd556a5cf
Fitsum <166730412+konjitfitsum@users.noreply.github.com>
2025-06-03 22:04:45 +0100

    Hero section add with 3d from spline

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Components/HEader";
+import HeroSection from "./Components/HeroSection";
 export default function App() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   );
 }
---
+++ src/Components/HeroSection.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { motion } from "framer-motion";
+import Spline from "@splinetool/react-spline";
+const HeroSection = () => {
+  return (
+    <section
+      className="h-screen bg-gradient-to-b
+    from-violet-900 to-black flex xl:flex-row 
+    flex-col-reverse items-center justify-between
+    lg:px-24 px-10 relative overflow-hidden
+    "
+    >
+      {/* left section */}
+      <div className="z-40 xl:mb-0 mb-[20%] ">
+        <motion.h1
+          initial={{ opacity: 0, y: 80 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 25,
+            delay: 1.3,
+            duration: 1.5,
+          }}
+          className="text-5xl md:text-7xl lg:text-8xl font-bold z-10 mb-6"
+        >
+          Building Fast <br /> Reliable Results
+        </motion.h1>
+        <motion.p
+          initial={{ opacity: 0, y: 80 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            type: "spring",
+            stiffness: 40,
+            damping: 25,
+            delay: 1.8,
+            duration: 1.5,
+          }}
+          className="text-xl md:text-1xl lg:text-2xl
+        text-purple-200 max-w-2xl
+        "
+        >
+          I deliver robust, Production-ready Websites and Web Apps with Speed
+          and precision .Every Project is backed by Clean Code,Clear
+          Communication, and a Commitment to getting it done, on time, Every
+          time.
+        </motion.p>
+      </div>
+      {/* right section */}
+      <Spline
+        className="absolute xl:right-[-28%]"
+        scene="https://prod.spline.design/teef3bWyxfJ0tfWP/scene.splinecode"
+      />
+    </section>
+  );
+};
+
+export default HeroSection;
